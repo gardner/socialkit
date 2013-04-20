7ff0fc7ea8a699b6e3c65ca46c399a45b42b7972
Pieter Gunst <pietergunst@gmail.com>
2013-04-20 14:08:45 -0700

    add basics page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,27 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130103194023) do
+ActiveRecord::Schema.define(:version => 20130420210452) do
+
+  create_table "basics", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "communities", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "organizations", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "resources", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string "email"
